838b3ea53d362a4c633473aa6c0d59e0d927dedc
1tpp <panapat.dev@gmail.com>
2023-04-28 17:02:17 +0700

    feat: base layout and meta data of website

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://1tpp.dev",
   output: "server",
   adapter: vercel()
 });
