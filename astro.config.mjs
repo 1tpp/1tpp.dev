622d24df623a91f1194d176fde0f33799c797ed4
1tpp <panapat.dev@gmail.com>
2023-04-28 19:37:46 +0700

    feat: simple astro pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,63 @@ import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://1tpp.dev",
-  output: "server",
-  adapter: vercel(),
+  // output: "server",
+  // adapter: vercel(),
   integrations: [
     tailwind(),
     sitemap({
       customPages: [`https://1tpp.dev/`, `https://1tpp.dev/about`],
     }),
+    compress(),
+    AstroPWA({
+      mode: "development",
+      base: "/",
+      scope: "/",
+      includeAssets: ["favicon.svg"],
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Astro PWA",
+        short_name: "Astro PWA",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: "/404",
+        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\/404$/],
+      },
+    }),
   ],
+  vite: {
+    logLevel: "info",
+    define: {
+      __DATE__: `'${new Date().toISOString()}'`,
+    },
+  },
 });
