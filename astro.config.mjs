6c602235eaccd3062b395d64c7f7dcfd9a6c0ff0
1tpp <panapat.dev@gmail.com>
2023-04-28 17:34:59 +0700

    feat: simple astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://1tpp.dev",
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    sitemap({
+      customPages: [`https://1tpp.dev/`, `https://1tpp.dev/about`],
+    }),
+  ],
 });
