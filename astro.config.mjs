9415937d9021a9ef2fe6d943dbb50c04262bf216
1tpp <panapat.dev@gmail.com>
2023-05-10 19:10:50 +0700

    chore: eslint and prettier setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 import AstroPWA from "@vite-pwa/astro";
