eaf7709bf413eccb1936566d953b00b168c8eeb6
1tpp <panapat.dev@gmail.com>
2023-05-13 02:24:31 +0700

    chore: change style

--- src/scripts/3d.js
+++ src/scripts/3d.js
@@ -8,23 +8,73 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("webgl").appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-
-scene.add(cube);
-camera.position.z = 5;
-
-const animate = function () {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
+// Mouse Effect movement on the screen
+const mouse = {
+  x: undefined,
+  y: undefined,
 };
+
+window.addEventListener("mousemove", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
+// Resize the screen
+window.addEventListener("resize", function () {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
+
+const particlesGeometry = new THREE.BufferGeometry();
+const particlesCnt = 5000;
+
+const posArray = new Float32Array(particlesCnt * 3);
+
+for (let i = 0; i < particlesCnt * 3; i++) {
+  posArray[i] = (Math.random() - 0.5) * 5;
+}
+
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(posArray, 3)
+);
+
+// Material
+const particlesMaterial = new THREE.PointsMaterial({
+  size: 0.005,
+});
+
+// Points
+const particlesMesh = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particlesMesh);
+
+// Lights
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+scene.add(pointLight);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 15;
+
+camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+// Render Loop
+function animate() {
+  requestAnimationFrame(animate);
+
+  particlesMesh.rotation.x = mouse.y * 0.0004;
+  particlesMesh.rotation.y = mouse.x * 0.0004;
+
+  renderer.render(scene, camera);
+}
 
 animate();
